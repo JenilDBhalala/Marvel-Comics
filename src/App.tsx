2ae052739f32f9cbdb7395f702c454940aa86730
rathodya <yagnik.rathod@synoverge.com>
2025-06-18 16:55:49 +0530

    feat: implement lazy loading for MovieList and MovieDetails components; update VITE_PUBLIC_URL in .env.example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
-import MovieDetails from "./components/MovieDetails/MovieDetails.tsx";
-import MovieList from "./components/MovieList/MovieList.tsx";
 import Footer from "./components/footer/Footer.tsx";
 import Header from "./components/header/Header.tsx";
 
+// Lazy-loaded components
+const MovieList = lazy(() => import("./components/MovieList/MovieList"));
+const MovieDetails = lazy(
+  () => import("./components/MovieDetails/MovieDetails")
+);
+
 function App() {
   const movieListSearchQuery = "movieListSearchQuery";
-  const basename = import.meta.env.VITE_PUBLIC_URL || '/';
+  const basename = import.meta.env.VITE_PUBLIC_URL || "/";
 
   // get searchQuery from local storage
   const getSearchQuery = (): string => {
@@ -18,33 +22,35 @@ function App() {
   const [searchQuery, setSearchQuery] = useState(getSearchQuery());
 
   return (
-    <BrowserRouter basename={`${basename}`}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="no-scrollbar">
-              <Header
-                searchQuery={searchQuery}
-                setSearchQuery={setSearchQuery}
-              />
-              <Outlet />
-              <Footer />
-            </div>
-          }
-        >
+    <BrowserRouter basename={basename}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
           <Route
-            index
+            path="/"
             element={
-              <MovieList
-                searchQuery={searchQuery}
-                movieListSearchQuery={movieListSearchQuery}
-              />
+              <div className="no-scrollbar">
+                <Header
+                  searchQuery={searchQuery}
+                  setSearchQuery={setSearchQuery}
+                />
+                <Outlet />
+                <Footer />
+              </div>
             }
-          />
-          <Route path="movie-details/:movieId" element={<MovieDetails />} />
-        </Route>
-      </Routes>
+          >
+            <Route
+              index
+              element={
+                <MovieList
+                  searchQuery={searchQuery}
+                  movieListSearchQuery={movieListSearchQuery}
+                />
+              }
+            />
+            <Route path="movie-details/:movieId" element={<MovieDetails />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
